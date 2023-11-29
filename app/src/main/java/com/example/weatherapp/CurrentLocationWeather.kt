9288836b3d5ed8907fa7e91599f72085bebe926c
Enara123 <silunienara@gmail.com>
2023-11-29 19:25:42 +0530

    first commit

---
+++ app/src/main/java/com/example/weatherapp/CurrentLocationWeather.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CurrentLocationWeather : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_location_weather)
+    }
+}
