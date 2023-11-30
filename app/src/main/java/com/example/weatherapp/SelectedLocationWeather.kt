f747529ed18efeebb40f4d5d15e6bb4a81284f6c
Enara123 <silunienara@gmail.com>
2023-11-30 22:32:05 +0530

    Minor fixes after pull

--- app/src/main/java/com/example/weatherapp/SelectedLocationWeather.kt
+++ app/src/main/java/com/example/weatherapp/SelectedLocationWeather.kt
@@ -21,35 +21,33 @@ import org.json.JSONArray
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-<<<<<<< HEAD
+
 class SelectedLocationWeather : AppCompatActivity() {
-=======
+
 class SelectedLocationWeather : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     //Weather
-    private lateinit var spnCity : Spinner
-    private lateinit var lblDescription : TextView
-    private lateinit var lblHumidity : TextView
-    private lateinit var lblPressure : TextView
-    private lateinit var lblTemperature : TextView
-    private lateinit var lblWindSpeed : TextView
-    private lateinit var imgWeather : ImageView
-    private val city = arrayOf("Colombo","London","Tokyo", "Hong Kong", "Manila", "Seoul", "Munich", "Paris")
+    private lateinit var spnCity: Spinner
+    private lateinit var lblDescription: TextView
+    private lateinit var lblHumidity: TextView
+    private lateinit var lblPressure: TextView
+    private lateinit var lblTemperature: TextView
+    private lateinit var lblWindSpeed: TextView
+    private lateinit var imgWeather: ImageView
+    private val city =
+        arrayOf("Colombo", "London", "Tokyo", "Hong Kong", "Manila", "Seoul", "Munich", "Paris")
 
     //RecyclerView for day forecast
-    private lateinit var forecastRecycles : RecyclerView
+    private lateinit var forecastRecycles: RecyclerView
     var forecastData = JSONArray()
->>>>>>> 657bbd5ae95a4b5bc2c4417bcee0b907eb648e7e
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selected_location_weather)
 
-<<<<<<< HEAD
-=======
         //Weather
-        spnCity=  findViewById(R.id.spn_city)
+        spnCity = findViewById(R.id.spn_city)
         lblDescription = findViewById(R.id.lbl_description)
         lblHumidity = findViewById(R.id.lbl_humidity)
         lblPressure = findViewById(R.id.lbl_pressure)
@@ -57,7 +55,7 @@ class SelectedLocationWeather : AppCompatActivity(), AdapterView.OnItemSelectedL
         lblWindSpeed = findViewById(R.id.lbl_windSpeed)
         imgWeather = findViewById(R.id.imageView)
 
-        val cityAdapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,city)
+        val cityAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, city)
         cityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spnCity.adapter = cityAdapter
@@ -68,7 +66,8 @@ class SelectedLocationWeather : AppCompatActivity(), AdapterView.OnItemSelectedL
         forecastRecycles.adapter = ForecastAdapter()
         forecastRecycles.layoutManager = LinearLayoutManager(
             this, LinearLayoutManager.VERTICAL,
-            false)
+            false
+        )
 
     }
 
@@ -86,18 +85,25 @@ class SelectedLocationWeather : AppCompatActivity(), AdapterView.OnItemSelectedL
     @SuppressLint("SetTextI18n")
     fun getCityWeatherInfo(city: String) {
         Log.e("API", "Api Called")
-        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=b07113e56b9d36ac3723fb77cfe44797"
+        val url =
+            "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=b07113e56b9d36ac3723fb77cfe44797"
         val request = JsonObjectRequest(
-            Request.Method.GET, url, null, {
-                    response ->
+            Request.Method.GET, url, null, { response ->
                 lblDescription.text =
-                    "Description : "+response.getJSONArray("weather").getJSONObject(0).getString("description")
-                lblTemperature.text = "Temperature : "+response.getJSONObject("main").getString("temp")
-                lblPressure.text = "Pressure : "+response.getJSONObject("main").getString("pressure")
-                lblHumidity.text = "Humidity : "+response.getJSONObject("main").getString("humidity")
-                lblWindSpeed.text = "Wind Speed : "+response.getJSONObject("wind").getString("speed")
+                    "Description : " + response.getJSONArray("weather").getJSONObject(0)
+                        .getString("description")
+                lblTemperature.text =
+                    "Temperature : " + response.getJSONObject("main").getString("temp")
+                lblPressure.text =
+                    "Pressure : " + response.getJSONObject("main").getString("pressure")
+                lblHumidity.text =
+                    "Humidity : " + response.getJSONObject("main").getString("humidity")
+                lblWindSpeed.text =
+                    "Wind Speed : " + response.getJSONObject("wind").getString("speed")
 
-                val imageURL = "https://openweathermap.org/img/w/" + response.getJSONArray("weather").getJSONObject(0).getString("icon")+".png"
+                val imageURL =
+                    "https://openweathermap.org/img/w/" + response.getJSONArray("weather")
+                        .getJSONObject(0).getString("icon") + ".png"
 
                 Picasso.get().load(imageURL).into(imgWeather)
             },
@@ -112,11 +118,12 @@ class SelectedLocationWeather : AppCompatActivity(), AdapterView.OnItemSelectedL
 
     //Fetching forecast data from API
     @SuppressLint("NotifyDataSetChanged")
-    private fun fetchForecastData (city : String) {
+    private fun fetchForecastData(city: String) {
         Log.e("API", "Api Called")
-        val url = "https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=b07113e56b9d36ac3723fb77cfe44797"
+        val url =
+            "https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=b07113e56b9d36ac3723fb77cfe44797"
         val request = JsonObjectRequest(
-            Request.Method.GET ,url,null,Response.Listener{ res ->
+            Request.Method.GET, url, null, Response.Listener { res ->
                 forecastData = res.getJSONArray("list")
                 forecastRecycles.adapter?.notifyDataSetChanged()
             },
@@ -128,33 +135,44 @@ class SelectedLocationWeather : AppCompatActivity(), AdapterView.OnItemSelectedL
     }
 
     //Setting up RecyclerView to fetch data
-    private inner class ForecastAdapter : RecyclerView.Adapter<ForecastDataHolder>(){
+    private inner class ForecastAdapter : RecyclerView.Adapter<ForecastDataHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastDataHolder {
-            val  view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.day_forcast,parent,false)
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.day_forcast, parent, false)
 
             return ForecastDataHolder(view)
         }
+
         override fun getItemCount(): Int {
             return forecastData.length()
         }
+
         @SuppressLint("SetTextI18n")
         override fun onBindViewHolder(holder: ForecastDataHolder, position: Int) {
             holder.forecastDay.text = forecastData.getJSONObject(position).getString("dt_txt")
 
-            val imageUrl = "https://openweathermap.org/img/w/" + forecastData.getJSONObject(position).getJSONArray("weather").getJSONObject(0).getString("icon")+".png"
+            val imageUrl =
+                "https://openweathermap.org/img/w/" + forecastData.getJSONObject(position)
+                    .getJSONArray("weather").getJSONObject(0).getString("icon") + ".png"
             Picasso.get().load(imageUrl).into(holder.dayImage)
 
-            holder.forecastDescription.text = forecastData.getJSONObject(position).getJSONArray("weather").getJSONObject(0).getString("description")
-            holder.forecastTemperature.text = String.format("%.1f",forecastData.getJSONObject(position).getJSONObject("main").getString("temp").toDouble() - 273.15 ) + " °C"
+            holder.forecastDescription.text =
+                forecastData.getJSONObject(position).getJSONArray("weather").getJSONObject(0)
+                    .getString("description")
+            holder.forecastTemperature.text = String.format(
+                "%.1f",
+                forecastData.getJSONObject(position).getJSONObject("main").getString("temp")
+                    .toDouble() - 273.15
+            ) + " °C"
         }
     }
 
-    private inner class ForecastDataHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        var forecastDay : TextView = itemView.findViewById(R.id.txt_day)
-        var dayImage : ImageView = itemView.findViewById(R.id.img_day)
-        var forecastDescription : TextView = itemView.findViewById(R.id.txt_description)
-        var forecastTemperature : TextView = itemView.findViewById(R.id.txt_temp)
->>>>>>> 657bbd5ae95a4b5bc2c4417bcee0b907eb648e7e
+    private inner class ForecastDataHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var forecastDay: TextView = itemView.findViewById(R.id.txt_day)
+        var dayImage: ImageView = itemView.findViewById(R.id.img_day)
+        var forecastDescription: TextView = itemView.findViewById(R.id.txt_description)
+        var forecastTemperature: TextView = itemView.findViewById(R.id.txt_temp)
+
+        }
     }
 }
