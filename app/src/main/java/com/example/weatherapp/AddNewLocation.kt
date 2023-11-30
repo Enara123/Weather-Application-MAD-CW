236cfd3862b7ba7928498b7d85ce826eae28924d
Enara123 <silunienara@gmail.com>
2023-11-30 22:35:50 +0530

    Add new location feature updated

--- app/src/main/java/com/example/weatherapp/AddNewLocation.kt
+++ app/src/main/java/com/example/weatherapp/AddNewLocation.kt
@@ -2,10 +2,35 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AddNewLocation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_location)
+
+        val database = FirebaseDatabase.getInstance()
+        val citiesRef = database.getReference("cities")
+
+        citiesRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val citiesList = ArrayList<City>()
+
+                for (citySnapshot in dataSnapshot.children) {
+                    val cityName = citySnapshot.getValue(String::class.java)
+                    val city = City(cityName ?: "")
+                    citiesList.add(city)
+                }
+
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                
+            }
+        })
     }
 }
